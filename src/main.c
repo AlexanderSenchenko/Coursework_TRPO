a657d27efdd584b16b8b244beba37936c18c6a5f
Александр <zayzsacha@mail.ru>
2017-04-29 12:55:55 +0700

    Start

--- src/main.c
+++ src/main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
 	FILE *Matrix;
-	//вводятся города которые будут использованны в функциях
-	//char *city_1, *city_2;
-	int matrix[5][5];
+	int matrix[5][5], arr_city[5];
 
 	Matrix = fopen("graph.txt", "r");
 	if (Matrix == NULL) {
 		return 0;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		fscanf(Matrix, "%d[^\n]", &arr_city[i]);	
 	}
 
 	for (int i = 0; i < 5; i++) {
@@ -18,7 +21,14 @@ int main()
 		}
 	}
 
+	printf("\t");
 	for (int i = 0; i < 5; i++) {
+		printf("%d\t", arr_city[i]);
+	}
+	printf("\n");
+
+	for (int i = 0; i < 5; i++) {
+		printf("%d\t", arr_city[i]);
 		for (int j = 0; j < 5; j++) {
 			printf("%d\t", matrix[i][j]);
 		}
